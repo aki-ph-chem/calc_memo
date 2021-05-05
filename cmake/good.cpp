e1f0ea7f22bd4ebeec5e8b4f173b40f97186fb12
aki <simizuakiralmn@gmail.com>
2021-05-05 18:29:36 +0900

    追加sample

---
+++ cmake/good.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "good.h"
+
+void good_mornig::GM(){
+
+    std::cout<<"good morning"<<std::endl;
+}
---
+++ cmake/good.h
@@ -0,0 +1,12 @@
+
+#ifndef _GOOD_H_
+#define _GOOD_H_
+
+class good_mornig{
+
+    public: void GM();
+};
+
+
+
+#endif // _GOOD_H_
