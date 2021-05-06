2e59bd47c43823123bc51e095bbd692902591871
aki <simizuakiralmn@gmail.com>
2021-05-07 00:56:20 +0900

    source codeが分散している場合

---
+++ cmake_2/src/souce.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+#include "class.h"
+
+void test::hello(){
+
+    std::cout<<"Hello World"<<std::endl;
+}
---
+++ cmake_2/test/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include"class.h"
+#include "good.h"
+
+int main(){
+
+    test T;
+    good_mornig G;
+
+    T.hello();
+
+    G.GM();
+
+    return 0;
+}
