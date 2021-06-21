8f1a34be70f6c9f15287d593b02979f9d4425ba5
aki <simizuakiralmn@gmail.com>
2021-06-21 20:42:33 +0900

    call bash from R lang bia sysytem()

---
+++ R_lang/hello.r
@@ -0,0 +1,23 @@
+#!/bin/R
+
+
+#print("Hello world")
+
+#for(i in 1:3){
+
+#system("./hello")
+
+#}
+
+
+func = function(n){
+
+                  for(i in 1:n){
+
+                               system("./hello")
+
+                              }
+                   }
+
+
+func(10)
